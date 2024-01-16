6a0d0329b512cdb0bfc9dc01f638fea1d2bd92e2
Reza Rezaei <59285547+rezarezaei1983@users.noreply.github.com>
2024-01-16 23:06:34 +0330

    Update SeparateFileByNanRatio_v7.py

--- SeparateFileByNanRatio_v7.py
+++ SeparateFileByNanRatio_v7.py
@@ -9,7 +9,6 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 """
 
-
 import os
 import sys
 import glob
@@ -56,33 +55,7 @@ class GetMaskedImage():
         source_ds = gdal.OpenEx(self.shapedir)
         src_band = source_ds.GetRasterBand(1)
         
-        ### get the layer
         source_layer = source_ds.GetLayer(0)   
-        """
-        ### get layer count
-        layer_count = source_ds.GetLayerCount()
-        print("\nlayer_count : \n", layer_count)
-        
-        ### get the layer name
-        layer = source_ds.GetLayerByIndex(0)
-        print('The layer is named: {n}\n'.format(n=layer.GetName()))
-           
-        ### get feature number
-        numFeatures = source_layer.GetFeatureCount()
-        print("\nnumFeatures : ", numFeatures)
-        
-        ### get a feature
-        feature = source_layer.GetFeature(0)
-        print("\nfeature : \n", feature)
-        
-        ### get attriute
-        attribute = feature.GetFieldAsString('ISO')
-        print("\nattribute : \n", attribute)
-        
-        ### get reference system of the shapefile
-        spatialRef = source_layer.GetSpatialRef()  
-        print("\nReference System : \n", spatialRef)
-        """
         number_of_bands = 1
         driver = gdal.GetDriverByName("MEM").Create(                           
             '', lon_size, lat_size, number_of_bands, gdal.GDT_Byte)
@@ -90,46 +63,13 @@ class GetMaskedImage():
         top_left_x = ulc_lon
         top_left_y = ulc_lat
         x_rotation, y_rotation = 0, 0
-        #y_rotation = 0 
         cell_width, cell_height = res, -res
-        #cell_height = -res
         geotransform = [top_left_x, cell_width, x_rotation, top_left_y, y_rotation, cell_height ]
         driver.SetGeoTransform(geotransform)  
-             
-        # Rasterize shapefile to grid
-        gdal.RasterizeLayer(driver, [1], source_layer, burn_values=[1], options=["ALL_TOUCHED=TRUE"])
-            
-        ### ****************************
-        #target_dsSRS = osr.SpatialReference()
-        #target_dsSRS.ImportFromEPSG(4326)
-        #driver.SetProjection(target_dsSRS.ExportToWkt())
-        ### ****************************
-        
+        gdal.RasterizeLayer(driver, [1], source_layer, burn_values=[1], options=["ALL_TOUCHED=TRUE"])        
         band = driver.GetRasterBand(1)
-        
-        # Get rasterized shapefile as numpy array
         array = band.ReadAsArray()
         array = np.flipud(array)    
-        """
-        ####################### Write Shapefile as TIFF #######################
-        proj = driver.GetProjection()
-        
-        #print("\nproj : \n", proj)
-        
-        driver2 = gdal.GetDriverByName("GTiff")  
-        
-        driver2.Register()
-        
-        outds = driver2.Create(
-            "C:/Users/Reza/Desktop/Deniz_data/testraster222.tif", 
-            lon_size, lat_size, number_of_bands, gdal.GDT_Byte) 
-        
-        #outds.SetGeoTransform(geotransform)  
-        #outds.SetProjection(proj)
-        outband = outds.GetRasterBand(1).WriteArray(array) 
-        #######################################################################
-        """
-        # Flush memory file
         driver = None
         band = None
            
@@ -148,32 +88,18 @@ class GetMaskedImage():
         lons = nc.variables['longitude'][:]
         lats = nc.variables['latitude'][:]
 
-        ### show the abs_aer_ind
-        plt.imshow(abs_aer_ind, origin='lower')  # , origin='lower'  
+        plt.imshow(abs_aer_ind, origin='lower')
         plt.title(cur_date)
         plt.show()
             
         lon_size = lons.size
         lat_size = lats.size
         cellsize = lons[:][1] - lons[:][0]
-            
-        ### create the mask
-        mask = self.makeMask(lons,lats,lon_size,lat_size,cellsize) 
-            
-        ### show the mask array
-        #plt.imshow(mask, origin='lower')
-        #plt.show()
-            
-        ### mask the abs_aer_ind data
-        mskd_abs_aer_ind = np.ma.masked_where(mask==0,abs_aer_ind)
-        #print("\nmskd_abs_aer_ind : \n", mskd_abs_aer_ind)
-            
-        ### count the total number of grids inside the non-masked region of array (including NaNs and non-NaNs)
-        non_mskd_grids = mskd_abs_aer_ind.count()
         
-        ### count the number on nans inside the non-masked region of array
+        mask = self.makeMask(lons,lats,lon_size,lat_size,cellsize) 
+        mskd_abs_aer_ind = np.ma.masked_where(mask==0,abs_aer_ind)
+        non_mskd_grids = mskd_abs_aer_ind.count()
         non_mskd_nans = np.count_nonzero(np.isnan(mskd_abs_aer_ind[~mskd_abs_aer_ind.mask]))            
-            
         nan_pernentage = (100 * non_mskd_nans) / non_mskd_grids
         print("\nnan_pernentage : ", nan_pernentage)
             
@@ -196,12 +122,8 @@ class GetMaskedImage():
 
 
 
-#    %%%%%%%%%%%%%%%%%%%%%%%%%      Instance      %%%%%%%%%%%%%%%%%%%%%%%%%
-
-
+#    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%      Run      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ins = GetMaskedImage(data_dir="C:/Users/Reza/Desktop/Deniz_data/Deniz_data_2/",
                      shapefile_dir="C:/Users/Reza/Desktop/Deniz_data/shapefile/TUR_adm0.shp",
                      max_nans_percent = 15)        
-
-#ins.CalculationOnMaskedArr()
 ins.RunCalculation()
